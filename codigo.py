e8bebf54a810d66b5456b446629c28ac9067d152
pedrolacombes <124370288+pedrolacombes@users.noreply.github.com>
2023-03-07 17:14:57 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -543,7 +543,7 @@ with tab3:
   sheet_name = 'Videos'
   url = 'https://docs.google.com/spreadsheets/d/'+sheet_id+'/gviz/tq?tqx=out:csv&sheet='+sheet_name
   data_videos = pd.read_csv(url)
-  data_videos = data_videos[data_videos['Jogador'] == select_player]
+  data_videos = data_videos[data_videos['Nome_Jogador'] == select_player]
     
   # Convertendo base de videos para dataframe
 
